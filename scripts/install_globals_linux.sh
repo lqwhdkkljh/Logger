c208b2d1c4666785803be0c1b474d332ee3e2590
Linus Willner <lwtechgaming@gmail.com>
2017-05-12 09:37:07 +0300

    Linting corrections

--- scripts/install_globals_linux.sh
+++ scripts/install_globals_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Installing global NPM modules required to run Logger..."
-sudo npm install -g babel-cli babel-preset-es2015 nodemon standard eslint eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node
+sudo npm install -g babel-cli babel-preset-es2015 nodemon standard eslint eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node eslint-plugin-react
 echo "Module installation ended."
--- scripts/install_globals_win.bat
+++ scripts/install_globals_win.bat
@@ -2,7 +2,7 @@
 
 ECHO Installing global NPM modules required to run Logger...
 
-npm install -g babel-cli babel-preset-es2015 nodemon standard eslint eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node
+npm install -g babel-cli babel-preset-es2015 nodemon standard eslint eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node eslint-plugin-react
 
 ECHO Module installation ended.
 
